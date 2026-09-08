4b25284b07518bb83f987771da20d4bdec1c6093
Hotta Sachie <hsachie@c2r8s5.42tokyo.jp>
2026-09-08 20:42:26 +0900

    a

--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -3,19 +3,39 @@ import math
 
 def get_player_pos() -> tuple:
     while True:
+        coordinate = input(
+            "Enter new coordinates as floats in format 'x,y,z': ")
+        coordinates = coordinate.split(",")
+
+        if len(coordinates) != 3:
+            print("Invalid syntax")
+            continue
         try:
-            coordinate = input(
-                "Enter new coordinates as floats in format 'x,y,z': ")
-            coordinates = coordinate.split(",")
-
-            if len(coordinates) != 3:
-                print("Invalid syntax")
-                continue
-
             x = float(coordinates[0])
             y = float(coordinates[1])
             z = float(coordinates[2])
             return (x, y, z)
-        except ValueError as err:
-            print(f"Error on parameter '{coordinates[1]}' : {err}")
+        except ValueError:
+            for parts in coordinates:
+                parts = parts.strip()
+                try:
+                    float(parts)
+                except ValueError as err:
+                    print(f"Error on parameter '{parts}': {err}")
+                    break
             continue
+
+
+if __name__ == "__main__":
+    print("=== Game Coordinate System ===")
+
+    print("Get a first set of coordinates")
+    pos1 = get_player_pos()
+    print(f"Got a first tuple: {pos1}")
+    print(f"It includes: x={pos1[0]}, y={pos1[1]}, z={pos1[2]}")
+    print(
+        f"Distance to center: {math.sqrt((pos1[0])**2 + (pos1[1])**2 + (pos1[2])**2):.4f}")
+    print("Get a second set of coordinates")
+    pos2 = get_player_pos()
+    print(f"Distance between the 2 sets of coordinates: "
+          f"{math.sqrt((pos2[0]-pos1[0])**2 + (pos2[1]-pos1[1])**2 + (pos2[2]-pos1[2])**2):.4f}")
---
+++ ex3/ft_achievement_tracker.py
@@ -0,0 +1,27 @@
+import random
+
+
+def gen_player_achievements() -> set:
+    achievements = ["First Steps", "Master Explorer", "Boss Slayer",
+                    "Collector Supreme", "World Savior", "Speed Runner",
+                    "Strategist", "Survivor", "Treasure Hunter", "Unstoppable",
+                    "Untouchable", "Sharp Mind", "Crafting Genius",
+                    "Hidden Path Finder"]
+
+    count = random.randint(7, 12)
+    select = random.sample(achievements, count)
+    return set(select)
+
+
+if __name__ == "__main__":
+    print("=== Achievement Tracker System ===")
+    Alice = gen_player_achievements()
+    Bob = gen_player_achievements()
+    Charlie = gen_player_achievements()
+    Dylan = gen_player_achievements()
+    print(f"Player Alice: {Alice}")
+    print(f"Player Bob: {Bob}")
+    print(f"Player Charlie: {Charlie}")
+    print(f"Player Dylan: {Dylan}")
+    print()
+    
